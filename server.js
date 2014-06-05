c8185e84c598d08817a128f00d78a3513cd2e7ff
Josh Graber <jgraber@renovo1.com>
2014-06-05 10:24:40 -0400

    Added stylus for CSS parsing and morgan for ExpressJS logging.

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
+// APP INIT
 var express = require('express');
 
+// Setup environment
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var environment = process.env.NODE_ENV;
 
 var app = express();
 
+
+// STYLUS
+var stylus = require('stylus');
+
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
+
+var stylusPreprocessor = stylus.middleware({
+		src: __dirname + '/public',
+		compile: compile,
+	});
+
+// VIEWS
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
+// LOGGING
+var morgan = require('morgan');
+var loggingProcessor = morgan();
+
+// ROUTING
+app.use(loggingProcessor);
+app.use(stylusPreprocessor);
+app.use(express.static(__dirname + '/public'));
 app.get('*', function(req, res) {
 	res.render('index');
 });
 
+// LAUNCH SERVER
 var port = 3030;
 
 app.listen(port);
