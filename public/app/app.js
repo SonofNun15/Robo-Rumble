fd6edd086a245e091f013dec97c91f8d2c196109
Josh Graber <jgraber@renovo1.com>
2014-06-12 12:16:24 -0400

    Removing the main controller, view, and routes from angular. The index page is not going to have any angular routes or complexity on it. It may have a login control, but otherwise it will be a simple landing page with links to other pages.

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,10 @@
 angular.module('roboRumble', ['ngRoute']);
 
-angular.module('roboRumble').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	$locationProvider.html5Mode(true);
-	$routeProvider.when('/', { templateUrl: '/partials/main', controller: 'mainMenuController' });
-}]);
+// angular.module('roboRumble').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	// $locationProvider.html5Mode(true);
+	// $routeProvider.when('/', { templateUrl: '/partials/main', controller: 'mainMenuController' });
+// }]);
 
-angular.module('roboRumble').controller('mainMenuController', ['$scope', function($scope) {
-	$scope.testVariable = "Main Menu Options!";
-}]);
+// angular.module('roboRumble').controller('mainMenuController', ['$scope', function($scope) {
+	// $scope.testVariable = "Main Menu Options!";
+// }]);
