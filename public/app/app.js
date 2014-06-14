1915d3751816a2238c015f285336c78e3c0bc1ec
Josh Graber <jgraber@renovo1.com>
2014-06-14 08:23:09 -0400

    Install ui-bootstrap, bootstrap library for angular.

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('roboRumble', ['ngRoute']);
+angular.module('roboRumble', ['ngRoute', 'ui.bootstrap']);
 
 angular.module('roboRumble').controller('mainMenuController', ['$scope', function($scope) {
 	$scope.testVariable = "Main Menu Options!";
