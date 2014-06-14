8d3fb7ebf7fce90af0b72ff994b823531695f045
Josh Graber <jgraber@renovo1.com>
2014-06-14 17:32:07 -0400

    Removing old unused controller and adding reference to sanitize.

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,1 @@
-angular.module('roboRumble', ['ngRoute', 'ui.bootstrap']);
-
-angular.module('roboRumble').controller('mainMenuController', ['$scope', function($scope) {
-	$scope.testVariable = "Main Menu Options!";
-}]);
+angular.module('roboRumble', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
