445f5b5e9f488c5785aeefdc3556ad6bc0a2ec9a
Josh Graber <jgraber@renovo1.com>
2014-08-23 13:13:32 -0400

    Replace truthy placeholder test with simple get instructions test on the robot.

--- tests/domain/robot.tests.js
+++ tests/domain/robot.tests.js
@@ -1,5 +1,17 @@
 describe ('Robot', function() {
-	it ('should be true', function() {
-		expect(true).to.be.true;
+	it ('should return the instructions that the robots modules support', function() {
+		var robot = new Robot();
+		var chassis = new BoxChassis();
+		chassis.modules.push(new SpeedyWheels());
+		
+		robot.chassis = chassis;
+		
+		var instructions = robot.getInstructionList();
+		
+		expect(instructions).to.have.length(4);
+		expect(instructions).to.contain(instruction.move2);
+		expect(instructions).to.contain(instruction.move3);
+		expect(instructions).to.contain(instruction.turnLeft);
+		expect(instructions).to.contain(instruction.turnRight);
 	});
 });
