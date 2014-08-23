6d9c4e9723dcf547ce9a1ab2f25be346229e69b2
SamGraber <samgraber@sundialproperties.com>
2014-08-23 15:36:15 -0400

    Added a function for generating a vector from an absolute point and a relative point. Added a function for adding a relativePoint to an absolute or relative point.

--- domain/Common/point.js
+++ domain/Common/point.js
@@ -3,3 +3,16 @@ function Point(x, y, z) {
 	this.y = y;
 	this.z = z;
 }
+
+Point.prototype.toVector = function(relativePoint) {
+	var vector = new Vector();
+	vector.origin = this;
+	vector.offset = relativePoint;
+	return vector;
+};
+
+Point.prototype.add = function(relativePoint) {
+	return new Point(this.x + relativePoint.x
+					, this.y + relativePoint.y
+					, this.z + relativePoint.z);
+};
