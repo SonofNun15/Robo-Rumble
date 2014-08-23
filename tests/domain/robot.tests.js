4d1a0edd4d036209ae5e51f4dc855b07eb451a13
Josh Graber <jgraber@renovo1.com>
2014-08-23 13:25:30 -0400

    Added a test to verify that duplicate instructions are not listed twice in the robot's instruction list.

--- tests/domain/robot.tests.js
+++ tests/domain/robot.tests.js
@@ -13,4 +13,29 @@ describe ('Robot', function() {
 		expect(instructions).to.contain(instruction.turnLeft);
 		expect(instructions).to.contain(instruction.turnRight);
 	});
+	
+	it ('should not return duplicate instructions when modules share the same instruction', function() {
+		var robot = new Robot();
+		robot.chassis = {
+			modules: [
+				{
+					getInstructionList: function() {
+						return [ instruction.move1, instruction.move2 ];
+					},
+				},
+				{
+					getInstructionList: function() {
+						return [ instruction.move2, instruction.move3 ];
+					},
+				}
+			],
+		};
+		
+		var instructions = robot.getInstructionList();
+
+		expect(instructions).to.have.length(3);
+		expect(instructions).to.contain(instruction.move1);
+		expect(instructions).to.contain(instruction.move2);
+		expect(instructions).to.contain(instruction.move3);
+	});
 });
