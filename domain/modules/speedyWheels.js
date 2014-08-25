01a9a0e14873e9d1e113f4aab3a3365fceb1021d
SamGraber <samgraber@sundialproperties.com>
2014-08-25 17:29:11 -0400

    Implemented execute in SpeedyWheels.

--- domain/modules/speedyWheels.js
+++ domain/modules/speedyWheels.js
@@ -16,3 +16,21 @@ SpeedyWheels.prototype.getInstructionList = function(complexityLevel) {
 	
 	return instructions;
 };
+
+SpeedyWheels.prototype.execute = function(action, map) {
+	if (action == instruction.move2) {
+		map.move(this.robot, this.robot.heading);
+		map.move(this.robot, this.robot.heading);
+	}
+	else if (action == instruction.move3) {
+		map.move(this.robot, this.robot.heading);
+		map.move(this.robot, this.robot.heading);
+		map.move(this.robot, this.robot.heading);
+	}
+	else if (action == instruction.turnRight) {
+		this.robot.heading = heading.clockwise(this.robot.heading);
+	}
+	else if (action == instruction.turnLeft) {
+		this.robot.heading = heading.counterclockwise(this.robot.heading);
+	}
+};
