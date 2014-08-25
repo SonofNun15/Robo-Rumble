472030bf689f69bb3f3e62c6b40ff9653243d87b
SamGraber <samgraber@sundialproperties.com>
2014-08-25 13:43:41 -0400

    Added a test to validate that only one item may be pushed by a single move.

--- tests/domain/map.tests.js
+++ tests/domain/map.tests.js
@@ -64,6 +64,48 @@ describe ('Map', function() {
 		expect(otherRobot.coordinate.z).to.equal(1);
 	});
 	
+	it ('should not push two robots', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(4, 4, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.east;
+		
+		var drive = new BasicWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var robot2 = new Robot();
+		robot2.coordinate = new Point(1, 0, 1);
+		robot2.heading = heading.south; //heading should be irrelevant when being pushed
+		
+		var robot3 = new Robot();
+		robot3.coordinate = new Point(2, 0, 1);
+		robot3.heading = heading.north; //heading should be irrelevant when being pushed
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		map.items.push(robot2);
+		map.items.push(robot3);
+		
+		drive.execute(instruction.move1, map);
+		
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(0);
+		expect(robot.coordinate.z).to.equal(1);
+		expect(robot2.coordinate.x).to.equal(1);
+		expect(robot2.coordinate.y).to.equal(0);
+		expect(robot2.coordinate.z).to.equal(1);
+		expect(robot3.coordinate.x).to.equal(2);
+		expect(robot3.coordinate.y).to.equal(0);
+		expect(robot3.coordinate.z).to.equal(1);
+	});
+	
 	it ('should not intersect with cube below', function() {
 		var vector = new Vector();
 		vector.origin = new Point(0.5, 0.5, 0.5);
