1042861ecbd2af24d19e93c6bd4fa4f5a09aa2bb
SamGraber <samgraber@sundialproperties.com>
2014-08-25 17:26:58 -0400

    Added tests to verify that move2, turnLeft, and uTurn function correctly.

--- tests/domain/modules/basicWheels.tests.js
+++ tests/domain/modules/basicWheels.tests.js
@@ -25,6 +25,32 @@ describe ('BasicWheels', function() {
 		expect(robot.coordinate.z).to.equal(1);
 	});
 	
+	it ('should move two spaces', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(3, 3, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.south;
+		
+		var drive = new BasicWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		
+		drive.execute(instruction.move2, map);
+		
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(2);
+		expect(robot.coordinate.z).to.equal(1);
+	});
+	
 	it ('should rotate right', function() {
 		var floor = new ConcreteBlock();
 		floor.coordinate = new Point(0, 0, 0);
@@ -45,6 +71,60 @@ describe ('BasicWheels', function() {
 		map.items.push(robot);
 		
 		drive.execute(instruction.turnRight, map);
+		
+		expect(robot.heading).to.equal(heading.south);
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(0);
+		expect(robot.coordinate.z).to.equal(1);
+	});
+	
+	it ('should rotate left', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(2, 2, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.west;
+		
+		var drive = new BasicWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		
+		drive.execute(instruction.turnLeft, map);
+		
+		expect(robot.heading).to.equal(heading.south);
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(0);
+		expect(robot.coordinate.z).to.equal(1);
+	});
+	
+	it ('should rotate 180 degrees', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(2, 2, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.north;
+		
+		var drive = new BasicWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		
+		drive.execute(instruction.uTurn, map);
 		
 		expect(robot.heading).to.equal(heading.south);
 		expect(robot.coordinate.x).to.equal(0);
