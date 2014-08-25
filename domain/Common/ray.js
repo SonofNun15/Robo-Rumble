ad4cc89cf856d6c7c62151fd49c1a0fcff0e5922
SamGraber <samgraber@sundialproperties.com>
2014-08-25 11:06:01 -0400

    sign array should contain values of 1 or 0.

--- domain/Common/ray.js
+++ domain/Common/ray.js
@@ -2,5 +2,8 @@ function Ray(vector) {
 	this.origin = vector.origin;
 	this.offset = vector.offset;
 	this.invoffset = vector.offset.inverse();
-	this.sign = [ this.invoffset.x < 0, this.invoffset.y < 0, this.invoffset.z < 0 ];
+	// 0 is positive, 1 is negative
+	this.sign = [ this.invoffset.x < 0 ? 1 : 0
+				, this.invoffset.y < 0 ? 1 : 0
+				, this.invoffset.z < 0 ? 1 : 0 ];
 }
