7f6758121991e11fa213bdf2ce9fec1d7eb49bac
SamGraber <samgraber@sundialproperties.com>
2014-08-25 17:35:04 -0400

    Added tests for the SpeedyWheels actions.

--- tests/domain/modules/speedyWheels.tests.js
+++ tests/domain/modules/speedyWheels.tests.js
@@ -19,4 +19,110 @@ describe('SpeedyWheels', function() {
 		expect(availableInstructions).to.contain(instruction.move2);
 		expect(availableInstructions).to.contain(instruction.move3);
 	});
+	
+	it ('should move two spaces', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(3, 3, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.south;
+		
+		var drive = new SpeedyWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		
+		drive.execute(instruction.move2, map);
+		
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(2);
+		expect(robot.coordinate.z).to.equal(1);
+	});
+	
+	it ('should move three spaces', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(4, 4, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.south;
+		
+		var drive = new SpeedyWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		
+		drive.execute(instruction.move3, map);
+		
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(3);
+		expect(robot.coordinate.z).to.equal(1);
+	});
+	
+	it ('should rotate right', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(2, 2, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.east;
+		
+		var drive = new SpeedyWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		
+		drive.execute(instruction.turnRight, map);
+		
+		expect(robot.heading).to.equal(heading.south);
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(0);
+		expect(robot.coordinate.z).to.equal(1);
+	});
+	
+	it ('should rotate left', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(2, 2, 0);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.west;
+		
+		var drive = new SpeedyWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(robot);
+		
+		drive.execute(instruction.turnLeft, map);
+		
+		expect(robot.heading).to.equal(heading.south);
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(0);
+		expect(robot.coordinate.z).to.equal(1);
+	});
 });
