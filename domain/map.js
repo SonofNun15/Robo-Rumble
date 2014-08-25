c5c979d411d77fa321b682ab8f082958e7686b5c
SamGraber <samgraber@sundialproperties.com>
2014-08-25 11:28:14 -0400

    Created an intersect function that checks to see whether a ray intersects with a cube. This will be used for collision detection.

--- domain/map.js
+++ domain/map.js
@@ -7,3 +7,32 @@ Map.prototype.move = function(movingItem, direction) {
 	//check for collision
 	movingItem.coordinate = movingItem.coordinate.add(direction);
 };
+
+Map.prototype.intersect = function(ray, cube) {
+	var bounds = [ cube.coordinate,
+					new Point(cube.coordinate.x + cube.size.x
+					, cube.coordinate.y + cube.size.y
+					, cube.coordinate.z + cube.size.z) ];
+
+    var tmin, tmax, tymin, tymax, tzmin, tzmax;
+    tmin = (bounds[ray.sign[0]].x - ray.origin.x) * ray.invoffset.x;
+    tmax = (bounds[1 - ray.sign[0]].x - ray.origin.x) * ray.invoffset.x;
+    tymin = (bounds[ray.sign[1]].y - ray.origin.y) * ray.invoffset.y;
+    tymax = (bounds[1 - ray.sign[1]].y - ray.origin.y) * ray.invoffset.y;
+    if ((tmin > tymax) || (tymin > tmax))
+        return false;
+    if (tymin > tmin)
+        tmin = tymin;
+    if (tymax < tmax)
+        tmax = tymax;
+    tzmin = (bounds[ray.sign[2]].z - ray.origin.z) * ray.invoffset.z;
+    tzmax = (bounds[1 - ray.sign[2]].z - ray.origin.z) * ray.invoffset.z;
+    if ((tmin > tzmax) || (tzmin > tmax))
+        return false;
+    if (tzmin > tmin)
+        tmin = tzmin;
+    if (tzmax < tmax)
+        tmax = tzmax;
+	
+	return true;
+};
