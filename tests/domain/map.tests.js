696990c8c6a0a3ff96a92908c91edca161eb207c
SamGraber <samgraber@sundialproperties.com>
2014-08-25 12:11:44 -0400

    Created a test to verify that a robot will collide with a wall and stop its movement.

--- tests/domain/map.tests.js
+++ tests/domain/map.tests.js
@@ -1,4 +1,43 @@
 describe ('Map', function() {
+	it ('should collide with wall', function() {
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = {
+			x: 2,
+			y: 2,
+			z: 0,
+		};
+		
+		var wall = new ConcreteBlock();
+		wall.coordinate = new Point(1, 0, 1);
+		wall.size = {
+			x: 0,
+			y: 1,
+			z: 1,
+		};
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 1);
+		robot.heading = heading.east;
+		
+		var drive = new BasicWheels(robot);
+		
+		robot.chassis = {
+			modules: [ drive ],
+		};
+		
+		var map = new Map();
+		map.items.push(floor);
+		map.items.push(wall);
+		map.items.push(robot);
+		
+		drive.execute(instruction.move1, map);
+		
+		expect(robot.coordinate.x).to.equal(0);
+		expect(robot.coordinate.y).to.equal(0);
+		expect(robot.coordinate.z).to.equal(1);
+	});
+	
 	it ('should not intersect with cube below', function() {
 		var vector = new Vector();
 		vector.origin = new Point(0.5, 0.5, 0.5);
