0501db9831dd6f6898c65956919d1c72e2cf9473
SamGraber <samgraber@sundialproperties.com>
2014-09-11 10:58:04 -0400

    Created an initial scheduler.

---
+++ domain/scheduler.js
@@ -0,0 +1,51 @@
+function Scheduler() {
+	this.map = {};
+}
+
+//the scheduler runs each of the robot turns, followed by the npcs, and finally the board elements.
+//then optionally the robots and npcs get another chance to take an end-of-turn action.
+//the scheduler has three public functions, initTurn, runNext and runAll.
+
+Scheduler.prototype.initTurn = function() {
+	//reinitialize the enumerators each turn, that will clean up any items that have been removed from the game
+	this.robotEnumerator = new ArrayEnumerator(this.map.getRobots());
+	this.npcEnumerator = new ArrayEnumerator(this.map.getNPCs());
+	this.boardElementEnumerator = new ArrayEnumerator(this.map.getRobots());	//enumerates robots to activate the board elements nearby
+};
+
+Scheduler.prototype.runNext = function() {
+	if (this.robotEnumerator.moveNext())
+	{
+		this.takeRobotTurn(this.robotEnumerator.current();
+	}
+	else if (this.npcEnumerator.moveNext())
+	{
+		this.takeNPCTurn(this.npcEnumerator.current();
+	}
+	else if (this.boardElementEnumerator.moveNext())
+	{
+		this.takeBoardElementTurn(this.npcEnumerator.current();
+	}
+	else
+	{
+		return false;
+	}
+	
+	return true;
+};
+
+//
+// Private methods
+//
+
+Scheduler.prototype.takeRobotTurn = function(robot) {
+
+};
+
+Scheduler.prototype.takeNPCTurn = function(npc) {
+
+};
+
+Scheduler.prototype.takeBoardElementTurn = function(boardElement) {
+
+};
