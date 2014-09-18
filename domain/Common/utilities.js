c47efb397df29e9455511d4cf4e655b2a96a0ea3
SamGraber <samgraber@sundialproperties.com>
2014-09-18 14:50:36 -0400

    Renamed sortByDescending to imply that it is only for numeric sort values.

--- domain/Common/utilities.js
+++ domain/Common/utilities.js
@@ -1,6 +1,6 @@
 utility = {};
 
-utility.sortByDescending = function(collection, callback, thisArg) {
+utility.sortByDescendingNumeric = function(collection, callback, thisArg) {
 	if (typeof callback == 'function')
 	{
 		return _.sortBy(collection, function(item) { return -callback(item, thisArg); });
