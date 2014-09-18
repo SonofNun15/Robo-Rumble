68c4b20a0c87028c0f5a7715e5fd656645cee43f
SamGraber <samgraber@sundialproperties.com>
2014-09-18 14:42:07 -0400

    Simplified sortByDescending.

--- domain/Common/utilities.js
+++ domain/Common/utilities.js
@@ -3,14 +3,6 @@ utility = {};
 utility.sortByDescending = function(collection, callback, thisArg) {
 	if (typeof callback == 'function')
 	{
-		collection.forEach(function(item) {
-			item.sortValue = -callback(item, thisArg);
-		});
-		
-		var sortedCollection = _.sortBy(collection, function(item) { return item.sortValue; });
-		
-		_.each(sortedCollection, function(item) { item.sortValue = null; });
-		
-		return sortedCollection;
+		return _.sortBy(collection, function(item) { return -callback(item, thisArg); });
 	}
 };
