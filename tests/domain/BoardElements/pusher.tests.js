b6d9d9c8b888a773c11cb5ce01d9d5a7792d5b80
SamGraber <samgraber@sundialproperties.com>
2014-09-23 17:47:53 -0400

    Since pushers push the same way robots do, a pusher shouldn't be able to push two robots at once.

--- tests/domain/BoardElements/pusher.tests.js
+++ tests/domain/BoardElements/pusher.tests.js
@@ -24,4 +24,36 @@ describe ('Pusher', function() {
 		expect(robot.coordinate.y).to.equal(1);
 		expect(robot.coordinate.z).to.equal(0);
 	});
+	
+	it ('should not push two robots', function() {
+		var pusher = new Pusher();
+		pusher.coordinate = new Point(0, 0, 0);
+		pusher.heading = heading.south;
+		pusher.activePhases = { '1': true };
+		
+		var floor = new ConcreteBlock();
+		floor.coordinate = new Point(0, 0, 0);
+		floor.size = new Size(3, 3, 0);
+		
+		var robot1 = new Robot();
+		robot1.coordinate = new Point(0, 0, 0);
+		robot1.heading = heading.east;
+		
+		var robot2 = new Robot();
+		robot2.coordinate = new Point(0, 1, 0);
+		robot2.heading = heading.south;
+		
+		var map = new Map();
+		map.items.push(pusher);
+		map.items.push(floor);
+		map.items.push(robot1);
+		map.items.push(robot2);
+		
+		expect(robot1.coordinate.x).to.equal(0);
+		expect(robot1.coordinate.y).to.equal(0);
+		expect(robot1.coordinate.z).to.equal(0);
+		expect(robot2.coordinate.x).to.equal(0);
+		expect(robot2.coordinate.y).to.equal(1);
+		expect(robot2.coordinate.z).to.equal(0);
+	});
 });
