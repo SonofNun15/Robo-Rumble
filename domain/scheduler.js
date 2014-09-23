0c2480162f4083c0c341e625ac8bdabdf374d38c
SamGraber <samgraber@sundialproperties.com>
2014-09-23 14:55:16 -0400

    Added functionality for running board elements in the scheduler.

--- domain/scheduler.js
+++ domain/scheduler.js
@@ -70,6 +70,23 @@ Scheduler.prototype.takeNPCTurn = function(npc) {
 
 };
 
-Scheduler.prototype.takeBoardElementTurn = function(boardElement) {
-
+Scheduler.prototype.takeBoardElementTurn = function(robot) {
+	// cycle through each boardElement priority level
+	for (int i = 9; i >= 0; i--)
+	{
+		var elements = _.filter(this.map.getBoardElements(), touchesRobotSpace);
+		_.each(elements, function(element) { 
+			if (utility.get(element.priority, i) == true)
+			{
+				element.execute(robot);
+			}
+		});
+	}
+	
+	function touchesRobotSpace(element) {
+		return robot.coordinate == element.coordinate
+				|| robot.coordinate == element.coordinate.add(new Point(1, 0, 0))	//touches the right side of the robot's space
+				|| robot.coordinate == element.coordinate.add(new Point(0, 1, 0))	//touches the south side of the robot's space
+				|| robot.coordinate == element.coordinate.add(new Point(0, 0, 1));	//touches the top of the robot's space
+	};
 };
