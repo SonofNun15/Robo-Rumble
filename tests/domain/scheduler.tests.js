70ae06a317150bf7b024c89aabcab41782279214
SamGraber <samgraber@sundialproperties.com>
2014-09-23 15:17:19 -0400

    Added a test for the scheduler running all elements that touch the robot's space.

--- tests/domain/scheduler.tests.js
+++ tests/domain/scheduler.tests.js
@@ -118,4 +118,49 @@ describe ('Scheduler', function() {
 		turns++;
 		this.turns++;
 	};
+	
+	it ('should run all board elements touching the current robot', function() {
+		turns = 0;
+		
+		var belt = new BoardElementEmulator();
+		belt.priority = { '6': true };
+		belt.coordinate = new Point(0, 0, 0);
+		
+		var pusher = new BoardElementEmulator();
+		pusher.priority = { '5': true };
+		pusher.coordinate = new Point(1, 0, 0);
+		
+		var crusher = new BoardElementEmulator();
+		crusher.priority = { '3': true };
+		crusher.coordinate = new Point(0, 0, 1);
+		
+		var robot = new Robot();
+		robot.coordinate = new Point(0, 0, 0);
+		
+		var map = new Map();
+		map.items.push(robot);
+		map.items.push(belt);
+		map.items.push(pusher);
+		map.items.push(crusher);
+		
+		var scheduler = new Scheduler(map);
+		scheduler.takeBoardElementTurn(robot);
+		
+		expect(turns).to.equal(3);
+		expect(belt.turns).to.equal(1);
+		expect(pusher.turns).to.equal(1);
+		expect(crusher.turns).to.equal(1);
+	});
+	
+	function BoardElementEmulator() {
+		MapItem.call(this);
+		this.type = mapItemType.boardElement;
+		this.priority = [];
+		this.turns = 0;
+	};
+	
+	BoardElementEmulator.prototype.execute = function() {
+		turns++;
+		this.turns++;
+	};
 });
