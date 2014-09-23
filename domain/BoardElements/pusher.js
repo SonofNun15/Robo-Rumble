b3fc4a4f88628d0aa1678a6447acf5af48e9b64e
SamGraber <samgraber@sundialproperties.com>
2014-09-23 16:36:58 -0400

    Pushers are only active on certain phases.

--- domain/BoardElements/pusher.js
+++ domain/BoardElements/pusher.js
@@ -5,16 +5,20 @@ function Pusher() {
 	this.size = new Size(1, 0, 0);	// size depends on the heading
 	this.heading = heading.south;
 	this.priority = { '5': true };
+	this.activePhases = {};
 }
 
-Pusher.prototype.execute = function(robot, map) {
+Pusher.prototype.execute = function(robot, map, phase) {
 	var currentCoordinate = this.coordinate;
 	
-	map.move(this, this.heading);
-	
-	if (!currentCoordinate.equals(this.coordinate))
+	if (utility.get(this.activePhases, phase) === true)
 	{
-		map.move(this, this.heading.inverse());
+		map.move(this, this.heading);
+		
+		if (!currentCoordinate.equals(this.coordinate))
+		{
+			map.move(this, this.heading.inverse());
+		}
 	}
 };
 
