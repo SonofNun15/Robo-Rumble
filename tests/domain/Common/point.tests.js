da3f38793a5b2bc85a14992d599fe162ae672944
SamGraber <samgraber@sundialproperties.com>
2014-10-17 17:48:59 -0400

    Added some tests for the new point functions.

--- tests/domain/Common/point.tests.js
+++ tests/domain/Common/point.tests.js
@@ -32,4 +32,24 @@ describe ('Point', function() {
 		var distance = point.distance();
 		expect(distance).to.equal(5);
 	});
+	
+	it ('should subtract (1, 2, 3) from (2, 3, 4) to get (1, 1, 1)', function() {
+		var point = new Point(2, 3, 4);
+		var otherPoint = new Point(1, 2, 3);
+		var result = point.subtract(otherPoint);
+		expect(result.x).to.equal(1);
+		expect(result.y).to.equal(1);
+		expect(result.z).to.equal(1);
+	});
+	
+	it ('should get the center of an object of size (2, 2, 2) as (1, 1, 1)', function() {
+		var item = {
+			coordinate: new Point(0, 0, 0),
+			size: new Point(2, 2, 2),
+		};
+		var center = item.coordinate.center(item);
+		expect(center.x).to.equal(1);
+		expect(center.y).to.equal(1);
+		expect(center.z).to.equal(1);
+	});
 });
